991e8c9545c9f6c62ca19e6c5415d285257a4402
EC2 Default User <ec2-user@ip-172-31-95-103.ec2.internal>
2022-01-08 14:51:18 +0000

    [Fix]favorite

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,11 +4,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(post_comment_params)
     comment.book_id = @book.id
     comment.save
+    render :book_comments
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back fallback_location: books_path
+    render :book_comments
   end
 
 private
